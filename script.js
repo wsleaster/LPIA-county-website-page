daa3b39c0fab0b6b7e04ae9cb477869e25791df2
Will <wsleaster@gmail.com>
2017-03-26 09:39:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$('#clickThis').click(function (){
+  $('#inputStuff').toggle();
+  
+});
+$('contactSubmit').click(function (){
+  alert('Sorry, this button is not yet mapped');
+  $("#form").trigger('reset');
+});
